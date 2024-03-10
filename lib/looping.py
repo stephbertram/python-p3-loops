7678c1ba00c514468c52c6782f452a35576edcbc
Stephanie Bertram <steph.a.bertram@gmail.com>
2024-03-09 17:23:03 -0800

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    countdown = 10
+    while countdown >= 1:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
+    
+# List Comprehension
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_numbers = [num ** 2 for num in int_list]
+    return squared_numbers
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+            
